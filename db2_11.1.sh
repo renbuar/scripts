6dd937f6792e5be8884620ebb641c2c48c389959
renbuar <31344382+renbuar@users.noreply.github.com>
2017-08-26 23:16:02 +0300

    Update db2_11.1.sh

--- db2_11.1.sh
+++ db2_11.1.sh
@@ -7,11 +7,11 @@ sudo groupadd db2fadm1
 sudo useradd -g db2fadm1 -m -d /home/db2fenc1 db2fenc1
 #sudo passwd db2fenc1
 sudo echo -e "pass\npass\n" | sudo passwd db2fenc1
-#sudo dpkg --add-architecture i386
-#sudo apt-get update
+sudo dpkg --add-architecture i386
+sudo apt-get update
+sudo apt-get install libpam0g:i386 -y
 #sudo apt-get install libaio1 -y
 sudo apt-get install libx32stdc++6 -y
-#sudo apt-get install libpam0g:i386 -y
 #sudo apt-get install binutils -y
 sudo apt-get install libnuma-dev -y
 sudo apt-get install libstdc++6 -y
