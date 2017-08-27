335b5bc18e818b38af5f4063f0c382d524cec16b
renbuar <31344382+renbuar@users.noreply.github.com>
2017-08-27 20:09:53 +0300

    Update db2_11.1.sh

--- ubuntu17.04/db2_11.1.sh
+++ ubuntu17.04/db2_11.1.sh
@@ -52,10 +52,13 @@ sudo systemctl start db2auto.service
 mkdir -p /tmp/1ctmp
 cd /tmp/1ctmp
 sudo apt install -y unixodbc libgsf-1-114
-sudo apt install ttf-mscorefonts-installer -y
+#sudo apt install ttf-mscorefonts-installer -y
 #wget  http://ftp.ru.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.6_all.deb
 #sudo  apt install -y xfonts-utils cabextract
 #sudo dpkg -i ttf-mscorefonts-installer_3.6_all.deb
+# фонты от Etersoft
+cp /home/user/Загрузки/fonts-ttf-ms_1.0-eter4ubuntu_all.deb /tmp/1ctmp
+sudo dpkg -i fonts-ttf-ms_1.0-eter4ubuntu_all.deb
 cp /home/user/Загрузки/deb64.tar.gz /tmp/1ctmp
 cp /home/user/Загрузки/client.deb64.tar.gz /tmp/1ctmp
 tar xvzf deb64.tar.gz
