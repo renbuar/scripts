9bf6f3f180c5dd5ef88bfbd98d686235365cc19c
renbuar <31344382+renbuar@users.noreply.github.com>
2017-08-28 17:07:09 +0300

    Update net.sh

--- net.sh
+++ net.sh
@@ -1,6 +1,5 @@
 # настройка ip debian 9 на kvm
-cd /tmp
-sudo cat > interfaces <<EOF
+sudo cat > /tmp/interfaces <<EOF
 # This file describes the network interfaces available on your system
 # and how to activate them. For more information, see interfaces(5).
 
@@ -21,8 +20,12 @@ gateway 192.168.0.1
 dns-nameservers 192.168.0.1
 auto ens3
 EOF
-sudo cp interfaces /etc/network
-#
+sudo cp /tmp/interfaces /etc/network
+sudo cat > /tmp/hosts <<EOF
+127.0.0.1       localhost
+127.0.1.1       debian
+EOF
+sudo cp /tmp/hosts /etc
 sudo /bin/su -c "echo 'net.ipv6.conf.all.disable_ipv6 = 1' >> /etc/sysctl.conf"
 sudo /bin/su -c "echo 'net.ipv6.conf.default.disable_ipv6 = 1' >> /etc/sysctl.conf"
 sudo /bin/su -c "echo 'net.ipv6.conf.lo.disable_ipv6 = 1' >> /etc/sysctl.conf"
