ded672476e89f8e5a007a032ba07775758cbc0b1
renbuar <31344382+renbuar@users.noreply.github.com>
2017-08-28 16:55:53 +0300

    Update net.sh

--- net.sh
+++ net.sh
@@ -1,16 +1,38 @@
-#Отключим службу ondemand
-(для разгона cpu)
+# настройка ip debian 9 на kvm
+cd /tmp
+sudo cat > interfaces <<EOF
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+# The primary network interface
+#auto ens3
+#iface ens3 inet dhcp
+
+iface ens3 inet static
+address 192.168.0.160
+netmask 255.255.255.0
+gateway 192.168.0.1
+dns-nameservers 192.168.0.1
+auto ens3
+EOF
+sudo cp interfaces /etc/network
+#
+sudo /bin/su -c "echo 'net.ipv6.conf.all.disable_ipv6 = 1' >> /etc/sysctl.conf"
+sudo /bin/su -c "echo 'net.ipv6.conf.default.disable_ipv6 = 1' >> /etc/sysctl.conf"
+sudo /bin/su -c "echo 'net.ipv6.conf.lo.disable_ipv6 = 1' >> /etc/sysctl.conf"
+#sudo /bin/su -c "echo 'net.ipv4.ip_forward = 1' >> /etc/sysctl.conf"
+sudo sysctl -p
+# ubuntu
+# Отключим службу ondemand
+# (для разгона cpu)
 # cat /proc/cpuinfo | grep MHz
 # systemctl status ondemand
 # systemctl stop ondemand
 # systemctl disable ondemand
 # nano /etc/sysctl.conf
-В конец
-!!!vm.swappiness=0
-!!!vm.vfs_cache_pressure = 100
-net.ipv6.conf.all.disable_ipv6 = 1
-net.ipv6.conf.default.disable_ipv6 = 1
-net.ipv6.conf.lo.disable_ipv6 = 1
-Сохранить.
-Применить сразу:
-# sysctl -p
