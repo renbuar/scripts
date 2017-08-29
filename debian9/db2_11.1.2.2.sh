d11375aec6cb60e0b5a5988424b144d10bde1875
renbuar <31344382+renbuar@users.noreply.github.com>
2017-08-29 19:08:48 +0300

    Update db2_11.1.2.2.sh

--- debian9/db2_11.1.2.2.sh
+++ debian9/db2_11.1.2.2.sh
@@ -34,43 +34,19 @@ sudo su db2inst1 -c '. /home/db2inst1/sqllib/db2profile;/home/db2inst1/sqllib/ad
 cd /tmp
 cat > db2auto.sh <<EOF
 #!/bin/sh
+#
 # Startup script for DB2 
 # Find the name of the script
-NAME=`basename $0`
-
 start() {
-    DB2_START=$"Starting ${NAME} service: "
     sudo su db2inst1 -c '. /home/db2inst1/sqllib/db2profile;/home/db2inst1/sqllib/adm/db2start'
-    ret=$? 
-    if [ $ret -eq 0 ]
-    then
-            echo "$DB2_START Success."
-    else
-            echo "$DB2_START Failed!"
-            exit 1
-    fi
-    echo
 }
-
 stop() {
-    echo -n $"Stopping ${NAME} service: "
     sudo su db2inst1 -c '. /home/db2inst1/sqllib/db2profile;/home/db2inst1/sqllib/adm/db2stop'
-    ret=$?
-    if [ $ret -eq 0 ]
-    then
-            echo "Success."
-    else
-            echo "Failed!"
-            exit 1
-    fi
-    echo
 }
-
 restart() {
     stop
     start
 }
-
 case "$1" in
     start)
         start
@@ -82,7 +58,7 @@ case "$1" in
         restart
         ;;
     *)
-        echo $"Usage: $0 {start|stop|restart}"
+        echo $"./db2auto.sh {start|stop|restart}"
         exit 1
 esac
 exit 0
